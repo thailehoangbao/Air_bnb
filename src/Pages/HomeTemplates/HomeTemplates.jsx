c67541a79bd9554d4c36608f3e8ef8f15b7b63cb
thailehoangbao <thaibao29111994@gmail.com>
2023-09-15 15:58:34 +0700

    header footer hometempates

--- src/Pages/HomeTemplates/HomeTemplates.jsx
+++ src/Pages/HomeTemplates/HomeTemplates.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './_components/Header/Header'
+import Footer from './_components/Footer/Footer'
 
 export default function HomeTemplates() {
     return (
-        <div>HomeTemplates</div>
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
     )
 }
