fa6927cae96a20530e08ecaf450512468c78e7dc
thailehoangbao <thaibao29111994@gmail.com>
2023-09-16 10:50:32 +0700

    push code homepage

--- src/Redux/rootReducers/rootReducers.jsx
+++ src/Redux/rootReducers/rootReducers.jsx
@@ -1,7 +1,11 @@
+import listPhongThueReducer from 'Pages/HomeTemplates/HomePage/ListPhongThue/duckListPhongThue/reducerListPhongThue';
+import viTriTimKiemReducer from 'Pages/HomeTemplates/HomePage/ListViTri/duckViTri/reducerViTri';
 import layDanhSachViTriReducer from 'Pages/HomeTemplates/_components/Header/HeaderMiddleTop/duckViTri/reducerDashboardViTri';
 import {combineReducers} from 'redux';
 const rootReducer = combineReducers({
-    layDanhSachViTriReducer: layDanhSachViTriReducer
+    layDanhSachViTriReducer: layDanhSachViTriReducer,
+    listPhongThueReducer: listPhongThueReducer,
+    viTriTimKiemReducer: viTriTimKiemReducer
 });
 
 export default rootReducer;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,7 +9,7 @@ const Routes = [
         nested: [
             {
                 path: '',
-                element: lazy(() => import('./../Pages/HomeTemplates/HomePage/HomePage'))
+                element: lazy(() => import('./../Pages/HomeTemplates/HomePage/Homepage'))
             },
             {
                 path: 'detail/:id',
