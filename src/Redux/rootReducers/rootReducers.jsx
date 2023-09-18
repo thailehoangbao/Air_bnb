aadca3a1e12663d2aab31ad02d4cab5bb5b68697
thailehoangbao <thaibao29111994@gmail.com>
2023-09-18 13:53:42 +0700

    update src thong tin ca nhan

--- src/Redux/rootReducers/rootReducers.jsx
+++ src/Redux/rootReducers/rootReducers.jsx
@@ -1,11 +1,15 @@
 import listPhongThueReducer from 'Pages/HomeTemplates/HomePage/ListPhongThue/duckListPhongThue/reducerListPhongThue';
 import viTriTimKiemReducer from 'Pages/HomeTemplates/HomePage/ListViTri/duckViTri/reducerViTri';
+import isOpenModalEditUserReducer from 'Pages/HomeTemplates/User/_components/duckEditUser/reducerEditUser';
+import listPhongDaDatUserReducer from 'Pages/HomeTemplates/User/duckListDatPhong/reducerUserDatPhong';
 import layDanhSachViTriReducer from 'Pages/HomeTemplates/_components/Header/HeaderMiddleTop/duckViTri/reducerDashboardViTri';
 import {combineReducers} from 'redux';
 const rootReducer = combineReducers({
     layDanhSachViTriReducer: layDanhSachViTriReducer,
     listPhongThueReducer: listPhongThueReducer,
-    viTriTimKiemReducer: viTriTimKiemReducer
+    viTriTimKiemReducer: viTriTimKiemReducer,
+    listPhongDaDatUserReducer: listPhongDaDatUserReducer,
+    isOpenModalEditUserReducer: isOpenModalEditUserReducer
 });
 
 export default rootReducer;
